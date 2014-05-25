e0d672f9e9310fcfe17927f54c5ef669b8ea6263
Denis Lebedev <lebedzeu@yandex-team.ru>
2014-05-25 10:07:35 +0300

    Initial Commit

---
+++ DLGraphView/DLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DLAppDelegate.h
+//  DLGraphView
+//
+//  Created by Dzianis Lebedzeu on 5/25/14.
+//  Copyright (c) 2014 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DLGraphView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DLGraphView
+//
+//  Created by Dzianis Lebedzeu on 5/25/14.
+//  Copyright (c) 2014 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DLAppDelegate class]));
+    }
+}
